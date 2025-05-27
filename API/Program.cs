4c05a1809a25f1a478d9266f57a0252023773662
AnnaNorlyk <148754654+AnnaNorlyk@users.noreply.github.com>
2025-05-27 21:23:30 +0200

    More mutation test coverage and logging config upped.

--- API/Program.cs
+++ API/Program.cs
@@ -1,12 +1,8 @@
 using Serilog;
 using Monitoring;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using API.Data;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
-using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 
 namespace API
 {
@@ -15,62 +11,72 @@ namespace API
         public static void Main(string[] args)
         {
             MonitorService.Initialize();
-            
             var logger = MonitorService.Log;
 
-            var builder = WebApplication.CreateBuilder(args);
-            builder.Host.UseSerilog(logger);
-
-            logger.Information("Seq sink is now configured and ready to receive events");
-
-
-            builder.Services.AddControllers();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-
-            builder.Services.AddCors(options =>
+            try
             {
-                options.AddPolicy("AllowFrontend", policy =>
+                logger.Information("Starting up...");
+
+                var builder = WebApplication.CreateBuilder(args);
+
+                builder.Logging.ClearProviders(); // Removes default providers
+                builder.Host.UseSerilog(logger);
+
+                // Services
+                builder.Services.AddControllers();
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen();
+
+                builder.Services.AddCors(options =>
                 {
-                    policy.WithOrigins(
-                            "http://localhost:3000",
-                            "http://141.147.1.249:3000"
-                        )
-                        .AllowAnyMethod()
-                        .AllowAnyHeader();
+                    options.AddPolicy("AllowFrontend", policy =>
+                    {
+                        policy.WithOrigins(
+                                "http://localhost:3000",
+                                "http://141.147.1.249:3000"
+                            )
+                            .AllowAnyMethod()
+                            .AllowAnyHeader();
+                    });
                 });
-            });
 
-            // Build connection string from environment variables
-            var dbHost = Environment.GetEnvironmentVariable("MYSQL_HOST");
-            var dbName = Environment.GetEnvironmentVariable("MYSQL_DATABASE");
-            var dbUser = Environment.GetEnvironmentVariable("MYSQL_USER");
-            var dbPass = Environment.GetEnvironmentVariable("MYSQL_PASSWORD");
+                // Database
+                var dbHost = Environment.GetEnvironmentVariable("MYSQL_HOST");
+                var dbName = Environment.GetEnvironmentVariable("MYSQL_DATABASE");
+                var dbUser = Environment.GetEnvironmentVariable("MYSQL_USER");
+                var dbPass = Environment.GetEnvironmentVariable("MYSQL_PASSWORD");
 
-            var connectionString = $"Server={dbHost};Database={dbName};User={dbUser};Password={dbPass};";
+                var connectionString = $"Server={dbHost};Database={dbName};User={dbUser};Password={dbPass};";
+                builder.Services.AddDbContext<TodoContext>(options =>
+                    options.UseMySql(connectionString, new MySqlServerVersion(new Version(10, 6))));
 
-            // Use the constructed connection string
-            builder.Services.AddDbContext<TodoContext>(options =>
-                options.UseMySql(connectionString, new MySqlServerVersion(new Version(10, 6))));
+                builder.Services.AddScoped<ITodoListService, TodoListService>();
 
-            builder.Services.AddScoped<ITodoListService, TodoListService>();
-            var app = builder.Build();
+                var app = builder.Build();
 
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+
+                app.UseCors("AllowFrontend");
+                app.UseHttpsRedirection();
+                app.UseAuthorization();
+                app.MapControllers();
+
+                app.Run();
             }
-
-            app.UseCors("AllowFrontend");
-
-            app.UseHttpsRedirection();
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.Run();
+            catch (Exception ex)
+            {
+                logger.Fatal(ex, "Application failed to start correctly");
+                throw;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
+
     }
 }
--- Monitoring/MonitoringService.cs
+++ Monitoring/MonitoringService.cs
@@ -23,11 +23,15 @@ namespace Monitoring
                          ?? throw new InvalidOperationException("SEQ_URL environment variable is not set.");
 
             Serilog.Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Verbose()
+                .MinimumLevel.Information() // general level
+                .MinimumLevel.Override("API.Services", LogEventLevel.Debug) //allows more detailed logging for specific namespaces
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("System", LogEventLevel.Warning)
                 .WriteTo.Console()
                 .WriteTo.Seq(serverUrl: seqUrl)
                 .Enrich.WithSpan()
                 .CreateLogger();
+
         }
     }
 }
